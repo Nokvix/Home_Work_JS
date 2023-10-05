8ecf33a446da76749933f44184677434fbd9c566
Nokvix <igor.gass2003@mail.ru>
2023-10-05 20:25:45 +0500

    Убрать закомментированный код

--- module6/task_2.js
+++ module6/task_2.js
@@ -1,10 +1,4 @@
 function filter(objects, property, value) {
-    let array = [];
-    // for (let object of objects) {
-    //     if (object[property] == value) {
-    //         array.push(object);
-    //     }
-    // }
     return objects.filter(object => object[property] === value);
 }
 
@@ -16,10 +10,4 @@ let objects = [
 
 let result = filter(objects, 'name', 'Иван');
 
-
-
-// console.log(result)
-
-for (let object of result) {
-    console.log(object);
-}
+console.log(result)
