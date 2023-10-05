bc249cd9cda2da0a7f42d05ff7b17d7ec1eb7935
Nokvix <igor.gass2003@mail.ru>
2023-10-05 17:08:47 +0500

    Решить задачу 2 модуль 6 в файле task_1.js

--- module6/task_1.js
+++ module6/task_1.js
@@ -2,13 +2,37 @@ function getOlderUser(user1, user2) {
     return user1.age > user2.age ? user1.name : user2.name;
 }
 
-let user1={
+let user1 = {
     name: "Игорь",
     age: 17
 };
-let user2={
+let user2 = {
     name: "Оля",
     age: 21
 };
+
 let result = getOlderUser(user1, user2);
-console.log(`Старший(-ая) - '${result}'`)
+console.log(`Старший(-ая) - '${result}'`);
+
+function getOlderUserArray(allUsers) {
+    let greatestAge = -1;
+    let oldestUser = {};
+    for (let user of allUsers) {
+        if (user.age > greatestAge) {
+            greatestAge = user.age;
+            oldestUser = user;
+        }
+    }
+    return oldestUser.name;
+}
+
+let allUsers = [
+    { name: 'Валя', age: 11 },
+    { name: 'Таня', age: 24 },
+    { name: 'Рома', age: 21 },
+    { name: 'Надя', age: 34 },
+    { name: 'Антон', age: 7 }
+];
+
+result = getOlderUserArray(allUsers);
+console.log(`Старший(-ая) - '${result}'`);
