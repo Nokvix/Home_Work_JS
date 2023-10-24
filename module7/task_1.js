fe1ea46a56a7c29e96148e32d0dbcb921da68ec6
Nokvix <igor.gass2003@mail.ru>
2023-10-24 20:26:36 +0500

    Решить задачу 1 модуль 7

---
+++ module7/task_1.js
@@ -0,0 +1,14 @@
+function createStudentCard(name, age) {
+    let div = document.createElement('div');
+    let h2 = document.createElement('h2');
+    let span = document.createElement('span')
+
+    document.body.append(div);
+    h2.textContent = name;
+    div.append(h2);
+
+    span.textContent = `Возраст: ${age} лет`;
+    div.append(span)
+}
+
+createStudentCard('Игорь', 19);
