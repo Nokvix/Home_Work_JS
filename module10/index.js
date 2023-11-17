16340101cda56c35fbca4780ad8d45ca93407ffc
Nokvix <igor.gass2003@mail.ru>
2023-11-18 00:12:32 +0500

    Сделать фильтры на заголовках

--- module10/index.js
+++ module10/index.js
@@ -2,193 +2,249 @@ let studentsArray = [];
 const date = new Date();
 const maxDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
 
-function defineTemporalData(date, placeholderText, minDate, maxDate) {
-    date.classList.add('form-control');
-    date.setAttribute('type', 'date');
-    date.placeholder = placeholderText;
-    date.min = minDate;
-    date.max = maxDate;
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const tableHeaders = document.querySelectorAll('.data-sort');
+    const tableBody = document.getElementById('table-body');
+    let sortOrder = 1;
 
-function zeroOutInputFields(studentForm) {
-    studentForm.nameInput.value = '';
-    studentForm.facultyInput.value = '';
-    studentForm.dataBirtInput.value = '';
-    studentForm.yearsStudyInput.value = '';
-}
+    tableHeaders.forEach(function (header, index) {
+        header.addEventListener('click', function () {
+            switch (index) {
+                case 0:
+                    handleSortClick('fullname');
+                    break;
+                case 1:
+                    handleSortClick('faculty');
+                    break;
+                case 2:
+                    handleSortClick('dateOfBirth');
+                    break;
+                case 3:
+                    handleSortClick('yearsStudy');
+                    break;
+            }
+            
+        });
+    });
 
-function formatDateString(dateString) {
-    const dateObject = new Date(dateString);
-    const day = dateObject.getDate();
-    const month = dateObject.getMonth() + 1;
-    const year = dateObject.getFullYear();
-    const currentDate = maxDate.split('-');
-    const currentYear = Number(currentDate[0]);
-    const currentMonth = Number(currentDate[1]);
-    const currentDay = Number(currentDate[2]);
-    let yearsOld;
+    function handleSortClick(columnIndex) {
+        tableBody.innerHTML = '';
 
-    if (currentMonth > month || currentMonth === month && currentDay >= day) {
-        yearsOld = currentYear - year;
-    } else {
-        yearsOld = currentYear - year - 1;
+        studentsArray.sort(function (a, b) {
+            const valueA = a[columnIndex];
+            const valueB = b[columnIndex];
+
+            if (valueA < valueB) return -sortOrder;
+            if (valueA > valueB) return sortOrder;
+            return 0;
+        });
+
+        studentsArray.forEach(function (student) {
+            let row = document.createElement('tr');
+
+            let name = document.createElement('td');
+            name.textContent = Array(student.surname, student.name, student.middleName).join(' ');
+
+            let faculty = document.createElement('td');
+            faculty.textContent = student.faculty;
+
+            let dataBirth = document.createElement('td');
+            dataBirth.textContent = formatDateString(student.dateOfBirth);
+
+            let yearsStudy = document.createElement('td');
+            yearsStudy.textContent = printYearsSchooling(student.yearsStudy);
+
+            row.appendChild(name);
+            row.appendChild(faculty);
+            row.appendChild(dataBirth);
+            row.appendChild(yearsStudy);
+
+            tableBody.appendChild(row);
+        });
     }
 
-    const formattedDay = (day < 10) ? `0${day}` : day;
-    const formattedMonth = (month < 10) ? `0${month}` : month;
-
-    return `${formattedDay}.${formattedMonth}.${year} (${yearsOld} лет)`;
-}
-
-function printYearsSchooling(dateString) {
-    const dateObject = new Date(dateString);
-    const yearStart = dateObject.getFullYear();
-    const yearFinish = yearStart + 4;
-    const currentDate = maxDate.split('-');
-    const currentYear = Number(currentDate[0]);
-    const currentMonth = Number(currentDate[1]);
-
-    if ((yearFinish < currentYear) || (currentYear === yearFinish && currentMonth > 9)) {
-        return `${yearStart}-${yearFinish} (закончил обучение)`
+    function defineTemporalData(date, placeholderText, minDate, maxDate) {
+        date.classList.add('form-control');
+        date.setAttribute('type', 'date');
+        date.placeholder = placeholderText;
+        date.min = minDate;
+        date.max = maxDate;
     }
 
-    if (currentMonth > 8) {
-        return `${yearStart}-${yearFinish} (${currentYear - yearStart + 1} курс)`
+    function zeroOutInputFields(studentForm) {
+        studentForm.nameInput.value = '';
+        studentForm.facultyInput.value = '';
+        studentForm.dataBirtInput.value = '';
+        studentForm.yearsStudyInput.value = '';
     }
 
-    return `${yearStart}-${yearFinish} (${currentYear - yearStart} курс)`
-}
+    function formatDateString(dateString) {
+        const dateObject = new Date(dateString);
+        const day = dateObject.getDate();
+        const month = dateObject.getMonth() + 1;
+        const year = dateObject.getFullYear();
+        const currentDate = maxDate.split('-');
+        const currentYear = Number(currentDate[0]);
+        const currentMonth = Number(currentDate[1]);
+        const currentDay = Number(currentDate[2]);
+        let yearsOld;
 
-function createStudent(studentForm) {
-    const nameArray = studentForm.nameInput.value
-        .split(/\s+/)
-        .filter(str => str.trim() !== '')
-        .map(str => str.trim());
+        if (currentMonth > month || currentMonth === month && currentDay >= day) {
+            yearsOld = currentYear - year;
+        } else {
+            yearsOld = currentYear - year - 1;
+        }
 
-    if (nameArray.length !== 3) {
-        return;
+        const formattedDay = (day < 10) ? `0${day}` : day;
+        const formattedMonth = (month < 10) ? `0${month}` : month;
+
+        return `${formattedDay}.${formattedMonth}.${year} (${yearsOld} лет)`;
     }
 
-    const facultyArray = studentForm.facultyInput.value
-        .split(/\s+/)
-        .filter(str => str.trim() !== '')
-        .map(str => str.trim());
+    function printYearsSchooling(dateString) {
+        const dateObject = new Date(dateString);
+        const yearStart = dateObject.getFullYear();
+        const yearFinish = yearStart + 4;
+        const currentDate = maxDate.split('-');
+        const currentYear = Number(currentDate[0]);
+        const currentMonth = Number(currentDate[1]);
 
-    const student = {
-        name: nameArray[1],
-        surname: nameArray[0],
-        middleName: nameArray[2],
-        faculty: facultyArray.join(' '),
-        dateOfBirth: new Date(studentForm.dataBirtInput.value),
-        yearsStudy: new Date(studentForm.yearsStudyInput.value),
-    };
+        if ((yearFinish < currentYear) || (currentYear === yearFinish && currentMonth > 9)) {
+            return `${yearStart}-${yearFinish} (закончил обучение)`
+        }
 
-    studentsArray.push(student);
+        if (currentMonth > 8) {
+            return `${yearStart}-${yearFinish} (${currentYear - yearStart + 1} курс)`
+        }
 
-    let row = document.createElement('tr');
+        return `${yearStart}-${yearFinish} (${currentYear - yearStart} курс)`
+    }
 
-    let name = document.createElement('td');
-    name.textContent = Array(student.surname, student.name, student.middleName).join(' ');
+    function createStudent(studentForm) {
+        const nameArray = studentForm.nameInput.value
+            .split(/\s+/)
+            .filter(str => str.trim() !== '')
+            .map(str => str.trim());
 
-    let faculty = document.createElement('td');
-    faculty.textContent = student.faculty;
+        if (nameArray.length !== 3) {
+            return;
+        }
 
-    let dataBirth = document.createElement('td');
-    dataBirth.textContent = formatDateString(studentForm.dataBirtInput.value);
+        const facultyArray = studentForm.facultyInput.value
+            .split(/\s+/)
+            .filter(str => str.trim() !== '')
+            .map(str => str.trim());
 
-    let yearsStudy = document.createElement('td');
-    yearsStudy.textContent = printYearsSchooling(studentForm.yearsStudyInput.value);
+        const student = {
+            name: nameArray[1],
+            surname: nameArray[0],
+            middleName: nameArray[2],
+            fullname: nameArray.join(' '),
+            faculty: facultyArray.join(' '),
+            dateOfBirth: new Date(studentForm.dataBirtInput.value),
+            yearsStudy: new Date(studentForm.yearsStudyInput.value),
+        };
 
-    row.appendChild(name);
-    row.appendChild(faculty);
-    row.appendChild(dataBirth);
-    row.appendChild(yearsStudy);
+        studentsArray.push(student);
 
-    return row;
-}
+        let row = document.createElement('tr');
 
-function createStudentForm() {
-    let form = document.createElement('form');
-    let nameInput = document.createElement('input');
-    let facultyInput = document.createElement('input');
-    let dataBirtInput = document.createElement('input');
-    let yearsStudyInput = document.createElement('input');
-    let button = document.createElement('button');
-    let buttonWrapper = document.createElement('div');
+        let name = document.createElement('td');
+        name.textContent = Array(student.surname, student.name, student.middleName).join(' ');
 
-    form.classList.add('input-group', 'mb-3');
+        let faculty = document.createElement('td');
+        faculty.textContent = student.faculty;
 
-    nameInput.classList.add('form-control');
-    nameInput.placeholder = 'Введите ФИО (Ивано Иван Иванович)';
+        let dataBirth = document.createElement('td');
+        dataBirth.textContent = formatDateString(studentForm.dataBirtInput.value);
 
-    facultyInput.classList.add('form-control');
-    facultyInput.placeholder = 'Введите название факультета'
+        let yearsStudy = document.createElement('td');
+        yearsStudy.textContent = printYearsSchooling(studentForm.yearsStudyInput.value);
 
-    defineTemporalData(dataBirtInput, 'Введите дату рождения', '1900-01-01', maxDate);
-    defineTemporalData(yearsStudyInput, 'Введите дату начала обучения', '2000-01-01', maxDate);
+        row.appendChild(name);
+        row.appendChild(faculty);
+        row.appendChild(dataBirth);
+        row.appendChild(yearsStudy);
 
-    button.classList.add('btn', 'btn-primary');
-    button.textContent = 'Добавить студента'
-    buttonWrapper.classList.add('input-group-append', 'd-block');
+        return row;
+    }
 
-    buttonWrapper.append(button);
-    form.append(nameInput);
-    form.append(facultyInput);
-    form.append(dataBirtInput);
-    form.append(yearsStudyInput);
-    form.append(buttonWrapper);
+    function createStudentForm() {
+        let form = document.createElement('form');
+        let nameInput = document.createElement('input');
+        let facultyInput = document.createElement('input');
+        let dataBirtInput = document.createElement('input');
+        let yearsStudyInput = document.createElement('input');
+        let button = document.createElement('button');
+        let buttonWrapper = document.createElement('div');
 
-    return {
-        form,
-        nameInput,
-        facultyInput,
-        dataBirtInput,
-        yearsStudyInput,
-        button,
-    };
-}
+        form.classList.add('input-group', 'mb-3');
 
-let studentForm = createStudentForm();
-let inputForm = document.getElementById('input-form');
-let tableBody = document.getElementById('table-body');
+        nameInput.classList.add('form-control');
+        nameInput.placeholder = 'Введите ФИО (Ивано Иван Иванович)';
 
-inputForm.append(studentForm.form);
+        facultyInput.classList.add('form-control');
+        facultyInput.placeholder = 'Введите название факультета'
 
-studentForm.nameInput.addEventListener('input', function () {
-    studentForm.facultyInput.addEventListener('input', function () {
-        studentForm.dataBirtInput.addEventListener('input', function () {
-            studentForm.yearsStudyInput.addEventListener('input', function () {
-                studentForm.button.disabled = !(
-                    studentForm.nameInput.value &&
-                    studentForm.facultyInput.value &&
-                    studentForm.dataBirtInput.value &&
-                    studentForm.yearsStudyInput.value
-                )
-            })
-        })
+        defineTemporalData(dataBirtInput, 'Введите дату рождения', '1900-01-01', maxDate);
+        defineTemporalData(yearsStudyInput, 'Введите дату начала обучения', '2000-01-01', maxDate);
+
+        button.classList.add('btn', 'btn-primary');
+        button.textContent = 'Добавить студента'
+        buttonWrapper.classList.add('input-group-append', 'd-block');
+
+        buttonWrapper.append(button);
+        form.append(nameInput);
+        form.append(facultyInput);
+        form.append(dataBirtInput);
+        form.append(yearsStudyInput);
+        form.append(buttonWrapper);
+
+        return {
+            form,
+            nameInput,
+            facultyInput,
+            dataBirtInput,
+            yearsStudyInput,
+            button,
+        };
+    }
+
+    function handleInputChange() {
+        studentForm.button.disabled = !(
+            studentForm.nameInput.value &&
+            studentForm.facultyInput.value &&
+            studentForm.dataBirtInput.value &&
+            studentForm.yearsStudyInput.value
+        );
+    }
+
+    let studentForm = createStudentForm();
+    let inputForm = document.getElementById('input-form');
+
+    inputForm.append(studentForm.form);
+
+    studentForm.nameInput.addEventListener('input', handleInputChange);
+    studentForm.facultyInput.addEventListener('input', handleInputChange);
+    studentForm.dataBirtInput.addEventListener('input', handleInputChange);
+    studentForm.yearsStudyInput.addEventListener('input', handleInputChange);
+
+    handleInputChange();
+
+    studentForm.form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        studentForm.button.disabled = true;
+
+        let row = createStudent(studentForm);
+
+        if (!row) {
+            studentForm.button.disabled = false;
+            alert('ФИО должно состоять из 3 слов. Если у вас сложное имя, то объединяйте его знаком "-" (тире)')
+            return;
+        }
+
+        tableBody.append(row);
+        zeroOutInputFields(studentForm);
     })
-})
-
-studentForm.button.disabled = !(
-    studentForm.nameInput.value &&
-    studentForm.facultyInput.value &&
-    studentForm.dataBirtInput.value &&
-    studentForm.yearsStudyInput.value
-)
-
-studentForm.form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    studentForm.button.disabled = true;
-
-    let row = createStudent(studentForm);
-
-    if (!row) {
-        studentForm.button.disabled = false;
-        alert('ФИО должно состоять из 3 слов. Если у вас сложное имя, то объединяйте его знаком "-" (тире)')
-        return;
-    }
-
-    tableBody.append(row);
-    zeroOutInputFields(studentForm);
-})
+});
